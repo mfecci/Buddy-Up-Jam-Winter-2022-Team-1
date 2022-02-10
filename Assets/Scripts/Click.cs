5cd62e29e4fbb01d33a26a423782659d74a0902a
mfecci <85564216+mfecci@users.noreply.github.com>
2022-02-10 20:25:54 +0100

    Random dust spawning

--- Assets/Scripts/Click.cs
+++ Assets/Scripts/Click.cs
@@ -7,11 +7,13 @@ using UnityEngine.SceneManagement;
 
 public class Click : MonoBehaviour
 {
-
+    public GameObject dust;
     public int stardust;
     public Text text;
-    private int augment = 1;
-
+    public int augment = 1;
+    public float spawnrate = 1;
+    private Vector2 screenBounds;
+    public float Timer = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +28,23 @@ public class Click : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Timer -= Time.deltaTime;
         text.text = stardust.ToString();
+        if (Timer <= 0f)
+        {
+            spawn();
+            Timer = spawnrate;
+        }
+    }
+
+    private void spawn()
+    {
+        GameObject a = Instantiate(dust) as GameObject;
+
+
+
+        float posy = Random.Range(-3, 3);
+        float posx = Random.Range(-5, 5);
+        a.transform.position = new Vector2(posx, posy);
     }
 }
